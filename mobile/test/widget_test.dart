4608d215e8a383d034d25cabdff7e761264f4436
Nelson Quiñinao <nquininao2023@alu.uct.cl>
2026-09-05 00:29:08 -0400

    feat(mobile):configure riverpod 3.x  with providerscope and initial AuthNotifier

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,30 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:apuntesuct_mobile/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Auth state Riverpod provider test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Initial state: not authenticated
+    expect(find.text('No autenticado'), findsOneWidget);
+    expect(find.text('Autenticado'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the toggle button to log in
+    await tester.tap(find.byType(FilledButton));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Updated state: authenticated
+    expect(find.text('Autenticado'), findsOneWidget);
+    expect(find.text('No autenticado'), findsNothing);
   });
 }
