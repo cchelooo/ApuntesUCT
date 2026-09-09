93a7d8ad67ea8159b35fc81bc4bc36c7020669e7
Antonio <alara2024@alu.uct.cl>
2026-09-08 23:19:14 -0300

    fix para npm run build

--- backend/api-gateway/test/app.e2e-spec.ts
+++ backend/api-gateway/test/app.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as supertest from 'supertest';
-import { Response } from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('API Gateway (e2e)', () => {
@@ -18,11 +17,10 @@ describe('API Gateway (e2e)', () => {
   });
 
   it('/api/v1/health (GET)', () => {
-    const agent = (supertest as unknown as (app: unknown) => supertest.SuperTest<supertest.Test>)(app.getHttpServer());
-    return agent
+    return request(app.getHttpServer())
       .get('/api/v1/health')
       .expect(200)
-      .expect((res: Response) => {
+      .expect((res) => {
         expect(res.body.status).toEqual('ok');
         expect(res.body.service).toEqual('API Gateway');
       });
