ae2d000c5efc612fa0020a906e31a21c445acab7
Antonio <alara2024@alu.uct.cl>
2026-09-14 22:08:09 -0300

    Creacion modelo jerarquico (Universidad, Carrera, Asignatura, Profesor)

---
+++ backend/catalog-service/src/application/dtos/create-subject.dto.ts
@@ -0,0 +1,23 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class CreateSubjectDto {
+  @IsUUID()
+  @IsNotEmpty()
+  careerId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  code!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  active?: boolean;
+}
---
+++ backend/catalog-service/src/domain/entities/index.ts
@@ -0,0 +1,4 @@
+export * from './university.entity';
+export * from './career.entity';
+export * from './subject.entity';
+export * from './professor.entity';
